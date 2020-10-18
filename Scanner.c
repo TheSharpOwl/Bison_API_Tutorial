ead05ae369a9ac6e7f5160d947355c9f089286a2
TheSharpOwl <ziadxkabakibi@hotmail.com>
2020-10-18 09:35:13 +0300

    Updated the scanner code.

--- Scanner.c
+++ Scanner.c
@@ -1,63 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-#include<parser.tab.h>
 
-int main()
+FILE* fptr;
+
+// returns 0 when the file is completely read
+int get_next_token()
 {
-   FILE* fptr;
-   fptr = fopen("input.txt", "r");
+    char s[1002], c;
+    int len = 0;
 
-   if(fptr == NULL)
-   {
-       printf("Error reading the file!\n");
-       exit(1);
-   }
-   char c = ' ';
-   char s[1002]; // let's suppose this length for a word is enough for now
-   int len = 0;
-   while(1)
-   {
-       c = fgetc(fptr);
-        if(c == ' ' || c == '\n' || c == EOF) // if the word is finished
+    while(1)
+    {
+        c = getc(fptr);
+
+        if(c == ' ' || c == '\n' || c == EOF)
         {
-            // add NULL termination at the end so that strcmp will know the ending of our array
-            s[len] = '\0'; 
+            if(len == 0) // we only have this character
+            {
+                if(c == EOF)
+                    return 0;
 
-            if(strcmp(s,"var") == 0) // the last word is var
-                printf("VAR");
-            else if(strcmp(s,"integer") == 0)
-                printf("INT");
-            else if(strcmp(s,"is") == 0)
-                printf("IS");
-            else if(len > 0)// it means it is some identifier (we don't care about misplacment for now)
-                printf("IDENT");
-
-            if(c != EOF)
+                // we can print it
                 printf("%c",c);
+                return 1;
+            }
             else
-                break;
-            //reset the char array
-            memset(s, 0, sizeof(s));
-            len = 0;
+            {
+            // add NULL termination at the end so that strcmp will know the ending of our array
+             s[len] = '\0'; 
 
+             if(strcmp(s,"var") == 0) // the last word is var
+                printf("VAR");
+             else if(strcmp(s,"integer") == 0)
+                printf("INT");
+             else if(strcmp(s,"is") == 0)
+                printf("IS");
+             else if(len > 0)// it means it is some identifier (if there's misplacment the grammar will discover it :D )
+                printf("IDENT");
+            // now we need to put the pointer exactly after the word (undo last step to reread ONLY the 'non letter or underscore')
+             ungetc(c, fptr);
+            //reset the char array
+             memset(s, 0, sizeof(s));
+             len = 0;
+             return 1;
+            }
         }
-        // if we have a letter then add it until we get to case 1 (space or new line)
-        else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_') 
+        else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_') // reading some name
         {
             s[len++] = c;
         }
         else
         {
-            // we won't stop if we find, just warn the user about it (we have a use of this in the next stage)
             printf("%s + Unknown symbol! ", s);
             //reset the char array
             memset(s, 0, sizeof(s));
             len = 0;
+            // stop everything 
+            return 0;
         }    
-   }
+    }
+}
+int main()
+{
+    fptr = fopen("input.txt", "r+");
 
-   fclose(fptr);
-   printf("\n");
+    while(get_next_token() != 0);
+
+    printf("\n");
+    
     return 0;
 }
