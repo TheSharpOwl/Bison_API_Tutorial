d1e764fcf41be7697a4f04c244ffc0ca0bf7e862
TheSharpOwl <ziadxkabakibi@hotmail.com>
2020-12-29 18:55:15 +0300

    I think I am done ^_^

--- Cpp/Main.cpp
+++ Cpp/Main.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-
+#include "parser.tab.hpp"
 // I know that global variables are often bad. Forgive me I wanna just explain the idea ((
 std::ifstream fin;
 
-std::string get_next_token()
+yy::parser::symbol_type get_next_token()
 {
     std::string s;
     char c;
-
     while (true)
     {
         // could be done better but organized it like this to edit only assignments and returns when using Bison API
         if (!fin.eof())
             fin.get(c);// get one character
-        else
-            return "";
+        else // return the end of the file so the parser will stop
+            return yy::parser::make_YYEOF();
 
         if (c == ' ' || c == '\n' || fin.eof())
         {
             if (s.empty()) // we only have this character
             {
                 if (fin.eof())
-                    return "";
+                    return yy::parser::make_YYEOF();
+                    
                 //otherwise go and see what's next
                 return get_next_token();
             }
@@ -36,13 +36,13 @@ std::string get_next_token()
                 }
 
                 if (s == "var") // the last word is var
-                    return s;
+                    return yy::parser::make_VAR();
                 if (s == "integer")
-                    return s;
+                    return yy::parser::make_INT();
                 if (s == "is")
-                    return s;
-                if (!s.empty())// it means it is some identifier name
-                    return  "Identifier";
+                   return yy::parser::make_IS();
+                if (!s.empty())// it means it is some identifier name 
+                   return yy::parser::make_IDENT(s); // don't forget to pass the identifier name stored in the string
             }
         }
         else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_') // reading some name
@@ -51,22 +51,23 @@ std::string get_next_token()
         }
         else
         {
-            // we don't know what's that
-            return "ERROR";
+            // we don't know what's that so return undefined token
+            return yy::parser::make_YYUNDEF();
         }
     }
 }
 int main()
 {
-
     fin.open("input.txt");
-    std::string temp = get_next_token();
-    while (!temp.empty())
-    {
-        // printing a space since our example is only one line for now
-        std::cout << temp << " ";
-        temp = get_next_token();
-
-    }
+    yy::parser p;
+	p.parse();
     return 0;
+}
+
+namespace yy
+{
+    parser::symbol_type yylex()
+    {
+        return get_next_token();
+    }
 }
